90a5f06640ef1096f4e42ed19bac329573810692
Ayushi Bose <ayushibose12@gmail.com>
2025-07-15 16:42:01 +0100

    changed to main py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Basic App to learn DevOps"
+    return "Basic App to learn DevOps - Ayushi"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
